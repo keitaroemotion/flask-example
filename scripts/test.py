1d5a00ef592d6cc008dda00d45e75ccd6452b830
Kei Sugano <kei.sugano@h-t.co.jp>
2020-07-05 20:42:36 +0900

    shebang removed

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from selenium                           import webdriver
 from selenium.webdriver.common.keys     import Keys
 from selenium.webdriver.firefox.options import Options
